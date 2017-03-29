9a612d8d9e872547fa1498fdcbbb14e69db43293
bassem <bassem.benamar@enis.tn>
2017-03-29 14:12:05 +0100

    entities

--- packager/src/main/java/tn/wevioo/PackagerApplication.java
+++ packager/src/main/java/tn/wevioo/PackagerApplication.java
@@ -1,9 +1,14 @@
 package tn.wevioo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
 @SpringBootApplication
+/*@EnableAutoConfiguration(exclude = {
+        JpaRepositoriesAutoConfiguration.class
+})*/
 public class PackagerApplication {
 
 	public static void main(String[] args) {
